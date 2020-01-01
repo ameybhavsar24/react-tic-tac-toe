f2f41849e2f68aff3c975630f9386d9f35bc636f
Amey Bhavsar <iamamey24@gmail.com>
2020-01-01 19:22:57 +0530

    Updates

---
+++ precache-manifest.d6807082d9b974965e494ec755e8cf06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1e60704349e1562f9826e7d45bc0858",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "6f4851f9d080353ee10e",
+    "url": "/react-tic-tac-toe/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "ee45084456e82309d982",
+    "url": "/react-tic-tac-toe/static/js/2.c1809de3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-tic-tac-toe/static/js/2.c1809de3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6f4851f9d080353ee10e",
+    "url": "/react-tic-tac-toe/static/js/main.a7a11a79.chunk.js"
+  },
+  {
+    "revision": "e9207ce51e19b5ed237e",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.d5188476.js"
+  }
+]);
